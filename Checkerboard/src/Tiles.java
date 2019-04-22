34d5b107e9b77008563d908991b9102db841add5
Benjamen Brooks <roflwhy@gmail.com>
2019-04-22 10:02:18 -0500

    Checkerboard Layout

---
+++ Checkerboard/src/Tiles.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Tiles {
+
+    public void paint(Graphics g) {
+
+    }
+
+}
